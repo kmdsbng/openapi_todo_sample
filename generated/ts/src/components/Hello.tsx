4a4dfa9fcc2ab0163f91eb386b5441a9196e90e9
Yoshihiro Kameda <kameda.sbng@gmail.com>
2020-05-04 15:32:09 +0900

    add typescript option

--- generated/ts/src/components/Hello.tsx
+++ generated/ts/src/components/Hello.tsx
@@ -1,4 +1,9 @@
 import * as React from "react";
+import {DefaultApi} from "../apis";
+import {Configuration} from "../runtime";
+export * from '../runtime';
+export * from '../apis';
+export * from '../models';
 
 export interface HelloProps { compiler: string; framework: string; }
 
@@ -6,6 +11,20 @@ export interface HelloProps { compiler: string; framework: string; }
 
 export class Hello extends React.Component<HelloProps, {}> {
     render() {
-        return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
+        return (<div>
+                <h1>Hello from {this.props.compiler} and {this.props.framework} edited!</h1>
+                <button onClick={this.onClickTodoList}>todo_list</button>
+            </div>
+        );
+    }
+
+    onClickTodoList = async () => {
+        const config = new Configuration({basePath: "http://127.0.0.1:4010"});
+        const api = new DefaultApi(config);
+
+        const {todos} = await api.getTodoList();
+
+        console.log(todos);
+
     }
 }
--- generated/ts/src/index.tsx
+++ generated/ts/src/index.tsx
@@ -1,6 +1,9 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import {Hello} from "./components/Hello";
+export * from './runtime';
+export * from './apis';
+export * from './models';
 
 ReactDOM.render(
     <Hello compiler={"TypeScript"} framework={"React"} />,
