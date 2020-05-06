35fbfeabeb5eb7552aa2909d64e3198acb90a76b
Yoshihiro Kameda <kameda.sbng@gmail.com>
2020-05-06 16:01:59 +0900

    fix generate code script

--- bin/generate_ts_code.sh
+++ bin/generate_ts_code.sh
@@ -1,6 +1,8 @@
 cd `dirname $0`
 cd ..
 
+mkdir -p generated/ts
+
 docker pull openapitools/openapi-generator-cli
 
 docker run --rm \
@@ -9,4 +11,21 @@ docker run --rm \
   -g typescript-fetch \
   --additional-properties=npmName='todo-api',typescriptThreePlus=true \
   -o /local/generated/ts
+
+
+mkdir -p frontend/src/
+mkdir -p frontend/.openapi-generator/
+
+cp -r generated/ts/src/models/ frontend/src/
+cp -r generated/ts/src/apis/ frontend/src/
+# cp generated/ts/src//index.ts frontend/src/
+cp generated/ts/src//runtime.ts frontend/src/
+cp -r generated/ts/README.md frontend/
+cp -n generated/ts/package.json frontend/
+cp -n generated/ts/tsconfig.json frontend/
+cp -n generated/ts/.npmignore frontend/
+cp -n generated/ts/.gitignore frontend/
+cp -r generated/ts/.openapi-generator-ignore frontend/
+cp -r generated/ts/.openapi-generator/VERSION frontend/.openapi-generator/
+
 
